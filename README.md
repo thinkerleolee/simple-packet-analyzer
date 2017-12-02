0bd25e353d63b794b49e37558d22a09c8f722f80
LeoLee <thinkerleo@hotmail.com>
2017-12-02 14:54:48 +0800

    更新的README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# simple-packet-analyzer-
+# simple-packet-analyzer
+
+## 介绍
+这是一个简单的多协议抓包软件，在学习完计算机网络后练手用，界面使用Qt编写
+
+## 功能
+- 多网卡选择
+ ![多网卡选择](https://github.com/thinkerleolee/-/blob/master/20171202143912.png?raw=true)
+
+- 支持多协议
+ ![支持多协议](https://github.com/thinkerleolee/-/blob/master/20171202143902.png?raw=true)
+
+ - 16进制/ASCII码查看
+ ![16进制/ASCII码查看](https://github.com/thinkerleolee/-/blob/master/20171202143925.png?raw=true)
+
+ ## 更新历史
+- Oct 17, 2017
+完成了初版，留下了一个GUI多线程无法结束的BUG，有待修复。
---
+++ build-arp_reciever-Desktop_Qt_5_9_3_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -0,0 +1,145 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../arp_reciever/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[11];
+    char stringdata0[213];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 17), // "startThreadListen"
+QT_MOC_LITERAL(2, 29, 0), // ""
+QT_MOC_LITERAL(3, 30, 16), // "stopThreadListen"
+QT_MOC_LITERAL(4, 47, 27), // "on_pushButton_start_clicked"
+QT_MOC_LITERAL(5, 75, 26), // "on_pushButton_stop_clicked"
+QT_MOC_LITERAL(6, 102, 28), // "on_tableWidget_doubleClicked"
+QT_MOC_LITERAL(7, 131, 5), // "index"
+QT_MOC_LITERAL(8, 137, 22), // "on_tableWidget_clicked"
+QT_MOC_LITERAL(9, 160, 47), // "on_comboBox_choose_protocol_c..."
+QT_MOC_LITERAL(10, 208, 4) // "arg1"
+
+    },
+    "MainWindow\0startThreadListen\0\0"
+    "stopThreadListen\0on_pushButton_start_clicked\0"
+    "on_pushButton_stop_clicked\0"
+    "on_tableWidget_doubleClicked\0index\0"
+    "on_tableWidget_clicked\0"
+    "on_comboBox_choose_protocol_currentIndexChanged\0"
+    "arg1"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    1,   53,    2, 0x08 /* Private */,
+       8,    1,   56,    2, 0x08 /* Private */,
+       9,    1,   59,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QModelIndex,    7,
+    QMetaType::Void, QMetaType::QModelIndex,    7,
+    QMetaType::Void, QMetaType::QString,   10,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->startThreadListen(); break;
+        case 1: _t->stopThreadListen(); break;
+        case 2: _t->on_pushButton_start_clicked(); break;
+        case 3: _t->on_pushButton_stop_clicked(); break;
+        case 4: _t->on_tableWidget_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 5: _t->on_tableWidget_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 6: _t->on_comboBox_choose_protocol_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
